950962274cf09ae2a14692ba5a728e511b1585d3
Максим <@DESKTOP-VHT1GOT>
2023-06-14 00:32:08 +0300

    Add project files.

---
+++ ProjectFileViewer/ViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
+namespace ProjectFileViewer
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private string folderPath;
+        public string FolderPath
+        {
+            get { return folderPath; }
+            set
+            {
+                folderPath = value;
+                OnPropertyChanged("FolderPath");
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void _openFileDialog()
+        {
+            CommonOpenFileDialog openFileDialog = new CommonOpenFileDialog();
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            openFileDialog.IsFolderPicker = true;
+
+            if (openFileDialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                //MessageBox.Show(openFileDialog.FileName);
+                FolderPath = openFileDialog.FileName;
+                //return FolderPath;
+            }
+            //return "0";
+        }
+    }
+}
